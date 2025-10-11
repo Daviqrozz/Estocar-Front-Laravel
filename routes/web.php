8d0ef73912a5e1a9c384a98ebb4b16270f8b2604
Davi <daviqrozz2@gmail.com>
2025-10-11 19:41:26 -0300

    feat:added auth pages templates

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/login',function (){
+    return view('auth/login');
+});
+Route::get('/register',function (){
+    return view('auth/register');
+});
+
+
 Route::get('/', function () {
     return view('dashboard/dashboard');
 });
+
 Route::get('/carros', function (){
     return view('carros/carros');
-})
+});
